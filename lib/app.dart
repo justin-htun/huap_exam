e99abcb04a9999d58a4d31f90cf8d2b6c5f8a6a6
Aung Naing Tun <>
2023-10-06 20:30:25 +0700

    Configure project structure

---
+++ lib/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:huap_exam/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HUAP',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+          iconTheme: IconThemeData(color: Colors.white, size: 23),
+          backgroundColor: Colors.blueAccent,
+          elevation: 0,
+          systemOverlayStyle:
+          SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
